f5ad6b777759d6053f3f6d38bd604cca8167c65f
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-24 20:04:35 +0900

    update: add support of uninstalling asdf plugins

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -23,14 +23,22 @@ else
   echo 'asdf is already installed'
 fi
 
-# Install asdf plugins
-while read -r plugin; do
-  if asdf plugin list | grep -q "$plugin"; then
-    echo "Plugin named $plugin already added"
+# Sync asdf plugins
+while read -r row; do
+  if [ -z "$row" ]; then
     continue
   fi
-  asdf plugin add "$plugin"
-done <./dotfiles/.config/asdf/plugins
+
+  if [[ "$row" =~ ^\+ ]]; then
+    plugin=$(echo "$row" | cut -b2-)
+    echo "Install plugin: $plugin"
+    asdf plugin add "$plugin"
+  elif [[ "$row" =~ ^\- ]]; then
+    plugin=$(echo "$row" | cut -b2-)
+    echo "Uninstall plugin: $plugin"
+    asdf plugin remove "$plugin"
+  fi
+done <<< "$(diff -U 100 <(asdf plugin list | sort -u) <(< ./dotfiles/.config/asdf/plugins sort -u) | sed -n '4,$p')"
 
 # Install git client (GitKraken)
 if ! command -v gitkraken; then
