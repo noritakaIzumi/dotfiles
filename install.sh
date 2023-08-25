806ab1ee809356efb745a9d2500be578c986ee34
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-26 02:06:01 +0900

    fix: remove unused variable

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ REPO_ROOT=$(cd "$(dirname "$0")" && pwd)
 
 # Create symlinks of config files
 cd "${REPO_ROOT}"/dotfiles || exit
-today=$(date '+%Y%m%d')
 find . -mindepth 1 -maxdepth 1 -print0 | while IFS= read -r -d '' file; do
   file=$(echo "$file" | sed 's/^.\///')
   if [[ $file = ".gitignore" ]]; then
